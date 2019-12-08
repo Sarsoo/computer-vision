284519c51d3e34799903cb8941b84142fc55f39e
aj <andrewjpack@gmail.com>
2019-12-08 03:13:45 +0000

    initial commit

---
+++ gui.py
@@ -0,0 +1,13 @@
+import cv2
+
+img = cv2.imread('nebula2.jpg', 0)
+
+cv2.imshow('image', img)
+k = cv2.waitKey(0)
+if k == 27:  # wait for ESC key to exit
+    cv2.destroyAllWindows()
+elif k == ord('s'):  # wait for 's' key to save and exit
+    cv2.imwrite('messigray.png', img)
+    cv2.destroyAllWindows()
+
+# cv2.imwrite('nebula2.jpg', img)
